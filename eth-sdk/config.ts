fefaed6d4728339788434fb5cbbbed99e918c810
wei3erHase <wei3erhase@defi.sucks>
2022-01-18 12:38:21 -0300

    fix: added ABI

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from '@dethcrypto/eth-sdk';
 
 export default defineConfig({
-	outputPath: 'src/eth-sdk-build',
-	contracts: {
-		mainnet: {
-			dca: '0xEcbA21E26466727d705d48cb0a8DE42B11767Bf7',
-		},
-	},
+  outputPath: 'src/eth-sdk-build',
+  contracts: {
+    mainnet: {
+      dca: '0xEcbA21E26466727d705d48cb0a8DE42B11767Bf7',
+    },
+  },
 });
